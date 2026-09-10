f21f8ee6b279099f7e7e1966e61bcd800d68ee12
thomas011001 <thomasyacoub321@gmail.com>
2026-09-10 15:50:37 +0300

    refactor: streamline message handling by encapsulating functions within MessageController class

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const path = require("node:path");
+const MessageController = require('./controllers/messageController')
+const MessageModel = require('./models/messageModel')
+
 const express = require("express");
 const app = express();
-
-const {
-  getAllMessages,
-  createMessage,
-  getOneMessage,
-} = require("./controllers/messageController");
 
 require("dotenv").config();
 const PORT = require("process").env.PORT || 8080;
@@ -19,15 +16,19 @@ app.use(express.urlencoded());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", getAllMessages);
-app.get("/message/:id", getOneMessage);
+
+const messageRepo = new MessageModel()
+const messageController = new MessageController(messageRepo)
+
+app.get("/", messageController.getAllMessages);
+app.get("/message/:id", messageController.getOneMessage);
 app.get("/new", (req, res) => res.render("new", { error: null }));
-app.post("/new", createMessage);
+app.post("/new", messageController.createMessage);
 
 app.use((err, req, res, next) => {
   console.error(err.name);
   const status = err.status || 500;
 
-  return res.status(404).render("error", { message: err.message });
+  return res.status(status).render("error", { message: err.message });
 });
 app.listen(PORT, () => console.log("started the server on: " + PORT));
