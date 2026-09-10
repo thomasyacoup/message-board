0765f652a1b2ecb6ab2d80724b1623fd8c39d1d6
thomas011001 <thomasyacoub321@gmail.com>
2026-09-10 15:50:26 +0300

    refactor: convert message handling functions to methods in MessageController and MessageModel classes

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,34 +1,38 @@
-const NotFoundError = require("../errors/NotFoundError");
-const Message = require("../models/messageModel");
-
-const getAllMessages = async (req, res, next) => {
-  const messages = await Message.getAll();
-  res.render("index", { messages });
-};
-
-const createMessage = async (req, res) => {
-  const text = req.body.text;
-  const user = req.body.user;
-  const date = req.body.date;
-
-  if (!date || !text || !user || text.trim() === "" || user.trim() === "") {
-    return res.status(400).render("new", {
-      error: "User, message and date are required.",
-    });
+class MessageController {
+  constructor(messageRepo) {
+    this.repo = messageRepo
   }
 
-  await Message.addMessage(text, user, date);
+  getAllMessages = async (req, res, next) => {
+    const messages = await this.repo.getAll();
+    res.render("index", { messages });
+  };
 
-  res.redirect("/");
-};
+  getOneMessage = async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const myMessage = await this.repo.getById(id);
+      res.render("message", { message: myMessage });
+    } catch (e) {
+      next(e)
+    }
+  };
 
-const getOneMessage = async (req, res) => {
-  const id = req.params.id;
-  const myMessage = await Message.getById(id);
-  if (!myMessage) {
-    throw new NotFoundError("Message Not Found");
-  }
-  res.render("message", { message: myMessage });
-};
+  createMessage = async (req, res) => {
+    const text = req.body.text;
+    const user = req.body.user;
+    const date = req.body.date;
+  
+    if (!date || !text || !user || text.trim() === "" || user.trim() === "") {
+      return res.status(400).render("new", {
+        error: "User, message and date are required.",
+      });
+    }
+  
+    await this.repo.addMessage(text, user, date);
+    
+    res.redirect("/");
+  };
+}
 
-module.exports = { getAllMessages, createMessage, getOneMessage };
+module.exports = MessageController;
--- models/messageModel.js
+++ models/messageModel.js
@@ -1,28 +1,34 @@
-const { v4: uuidv4 } = require("uuid");
+const uuid = require("uuid");
+const NotFoundError = require("../errors/NotFoundError");
 
-const messages = [
-  {
-    id: uuidv4(),
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    id: uuidv4(),
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+class MessageModel {
+  #messages = [
+    {
+      id: uuid.v4(),
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date(),
+    },
+    {
+      id: uuid.v4(),
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date(),
+    },
+  ];
+  
+  getAll = async () => this.#messages;
+  
+  getById = async (id) => {
+    this.#messages.find((item) => item.id === id);
+    if (!message) throw new NotFoundError("There's no message with this id.")
+    return message;
+  }
+  
+  addMessage = async (text, user, date) => {
+    const message = { id: uuid.v4(), text, user, added: new Date(date) };
+    this.#messages.push(message);
+  };
+}
 
-const getAll = async () => messages;
-
-const getById = async (id) => messages.find((item) => item.id === id);
-
-const addMessage = async (text, user, date) => {
-  const message = { id: uuidv4(), text, user, added: new Date(date) };
-
-  messages.push(message);
-};
-
-module.exports = { getAll, getById, addMessage };
+module.exports = MessageModel;
